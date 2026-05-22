e728f66a7af7fc55418f924bc913c3e5c639dbec
Yevhen Pashkulskyi <9349148@stud.op.edu.ua>
2026-05-22 18:19:49 +0300

    created Photgrapher DAO

---
+++ src/main/java/com/example/dataDB/storage/PhotographerDAO.java
@@ -0,0 +1,42 @@
+package com.example.dataDB.storage;
+
+import com.example.dataDB.DataBaseConnection;
+import com.example.entity.Photographer;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class PhotographerDAO {
+
+    public void savePhotographer(Photographer photographer) {
+
+        String sql = """
+                insert into photographer(fullName, phone, specialization, baseRate)
+                values(?, ?, ?, ?, ?)
+                """;
+        try(Connection connection = DataBaseConnection.getConnection();
+            PreparedStatement psmt = connection.prepareStatement(sql)){
+
+            psmt.setString(1, photographer.getFullName());
+            psmt.setString(2, photographer.getPhone());
+            psmt.setString(3, photographer.getSpecialization());
+            psmt.setDouble(4, photographer.getBaseRate());
+
+        }catch(SQLException e){
+            System.err.println("Помилка при збереженні фотографа: " + e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void createTablePhotographer() {
+        String sql = """
+            create table if not exists photographer(
+            id bigSerial primary key,
+            fullName varchar(255) not null,
+            phone varchar(255) not null,
+            specialization varchar(255) not null,
+            baseRate decimal not null);
+        """;
+    }
+}
