0f998d80822a2deedc6add0f77f4f81d4fa874ff
Yevhen Pashkulskyi <9349148@stud.op.edu.ua>
2026-05-25 23:54:38 +0300

    create method "update client" and "get clients"

--- src/main/java/com/example/dataDB/storage/ClientDAO.java
+++ src/main/java/com/example/dataDB/storage/ClientDAO.java
@@ -3,9 +3,10 @@ package com.example.dataDB.storage;
 import com.example.dataDB.DataBaseConnection;
 import com.example.entity.Client;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClientDAO {
 
@@ -15,7 +16,7 @@ public class ClientDAO {
                 VALUES (?, ?, ?, ?, ?)
                 """;
         try(Connection conn = DataBaseConnection.getConnection();
-            PreparedStatement pstmt = conn.prepareStatement(sql)){
+            PreparedStatement pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
 
             pstmt.setString(1, client.getFullName());
             pstmt.setString(2, client.getPhone());
@@ -24,7 +25,17 @@ public class ClientDAO {
             pstmt.setDouble(5, client.getDiscountRate());
 
             pstmt.executeUpdate();
-            System.out.println("Клієнта успішно збережено в БД!");
+
+            // Витягуємо ID клієнта
+            try (var generatedKeys = pstmt.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    long generatedId = generatedKeys.getLong(1);
+                    client.setId(generatedId); // Записуємо ID в об'єкт (переконайся, що клас Person має метод setId)
+                    System.out.println("Клієнта успішно збережено в БД з ID: " + generatedId);
+                } else {
+                    throw new SQLException("Не вдалося отримати згенерований ID для клієнта.");
+                }
+            }
 
         }catch (SQLException e){
             System.err.println("Помилка при збереженні клієнта: " + e.getMessage());
@@ -32,7 +43,50 @@ public class ClientDAO {
         }
     }
 
-    private void createTableClient() {
+    public void updateClient(Client client) {
+        String sql = """
+                update clients
+                set is_regular_client = ?
+                set discount_rate = ?
+                where id = ?
+                """;
+        try(Connection connection = DataBaseConnection.getConnection();
+        PreparedStatement pstmt = connection.prepareStatement(sql)){
+
+            pstmt.setBoolean(1, client.isRegular());
+            pstmt.setDouble(2, client.getDiscountRate());
+
+            pstmt.executeUpdate();
+            System.out.printf("Данні %s успішно оновлені", client.getFullName());
+
+        }catch (SQLException e){
+            System.err.println("Не вдалось оновити данні! " + e.getMessage());
+        }
+    }
+
+    public List<Client> getClients() {
+        List<Client> list = new ArrayList<>();
+        String sql = "SELECT * FROM clients";
+        try (Connection conn = DataBaseConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery()) {
+            while (rs.next()) {
+                list.add(new Client(
+                        rs.getLong("id"),
+                        rs.getString("full_name"),
+                        rs.getString("phone"),
+                        rs.getString("email"),
+                        rs.getBoolean("is_regular_client"),
+                        rs.getDouble("discount_rate")
+                ));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public void createTableClient() {
         String sql = """
                 CREATE TABLE IF NOT EXISTS clients (
                 id bigserial PRIMARY KEY,
@@ -43,7 +97,13 @@ public class ClientDAO {
                 discount_rate decimal(5,2) not null
                 );
                 """;
-        try {} catch (Exception e) {
+        try(Connection connection = DataBaseConnection.getConnection();
+            Statement stmt = connection.createStatement()){
+
+            stmt.execute(sql);
+            System.out.println("Таблиця створена");
+        }catch (SQLException e){
+            System.err.println("Помилка при створенні таблиці: " + e.getMessage());
             throw new RuntimeException(e);
         }
     }
