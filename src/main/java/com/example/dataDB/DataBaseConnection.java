9c6a12df5bf5fa601b0a4c2eb818e9f1cf58c4aa
Yevhen Pashkulskyi <9349148@stud.op.edu.ua>
2026-05-26 19:00:36 +0300

    modified, added an environment variable

--- src/main/java/com/example/dataDB/DataBaseConnection.java
+++ src/main/java/com/example/dataDB/DataBaseConnection.java
@@ -7,14 +7,13 @@ import java.sql.SQLException;
 public class DataBaseConnection {
 
     // Вказуємо адресу нашої бази даних PostgreSQL
-    // Зміни "photo_studio_db" на реальну назву твоєї бази даних
-    private static final String URL = "";
+    private static final String URL = System.getenv().getOrDefault("DB_URL","jdbc:postgresql://localhost:5432/DB");
 
     // логін у PostgreSQL
-    private static final String USER = "";
+    private static final String USER = System.getenv("DB_USER");
 
     // пароль від бази даних
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
     /**
      * Метод для отримання підключення до бази даних.
@@ -22,6 +21,9 @@ public class DataBaseConnection {
      * @throws SQLException якщо підключення не вдалося
      */
     public static Connection getConnection() throws SQLException {
+        if (USER == null || PASSWORD == null) {
+            throw new SQLException("Помилка конфігурації: Змінні середовища DB_USER або DB_PASSWORD не встановлені!");
+        }
         // DriverManager сам знайде потрібний драйвер і відкриє з'єднання
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
